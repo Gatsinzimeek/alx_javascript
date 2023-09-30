d707789458f89a44b3a3300534537626e18ef076
Gatsinzimeek <87667335+Gatsinzimeek@users.noreply.github.com>
2023-09-30 18:21:56 +0300

    Update 2-rectangle.js

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -8,12 +8,4 @@ class Rectangle{
   }
 };
 
-const r2 = new Rectangle(2,-3);
-console.log(r2);
-console.log(r2.width);
-console.log(r2.height);
-<<<<<<< HEAD
-
-console.log(typeof(r2))
-=======
->>>>>>> 7e13f91aef1a884801123b1c26860b5145a8e243
+module.exports = Rectangle;
