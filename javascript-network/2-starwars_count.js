1382fc580f0ee8c73f6b17ec2253361ad9b68232
Gatsinzimeek <hackergastinzi@gmail.com>
2023-10-31 13:35:04 +0200

    count

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,17 +1,24 @@
+const request = require('request');
 
-const req = require('request');
+// API URL for Star Wars films
+const apiUrl = process.argv[2]; // Pass this as the first command-line argument
 
-const Urlarg = process.argv[2];
+// Character ID for Wedge Antilles
 const characterId = 18;
 
-req.get(Urlarg,(error,res,body)=>{
-    if(error){
-        console.log('Error occur: ',error)
-    }else{
-        const films = JSON.parse(body).results;
-        let count = films.forEach(film => {
-            film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-        });
-        console.log(count.length);
-    }
+// Make an HTTP request to the API
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const films = JSON.parse(body).results;
+
+    // Filter the films where Wedge Antilles (character ID 18) is present
+    const filmsWithWedgeAntilles = films.filter(film =>
+      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+    );
+
+    // Print the count of films with Wedge Antilles
+    console.log(`Number of films with Wedge Antilles: ${filmsWithWedgeAntilles.length}`);
+  } else {
+    console.error('Error:', error);
+  }
 });
